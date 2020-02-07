f2f11e559cf2a569ea9901e0ea026336230cd59b
Armelle <armelle.lecerf@orange.fr>
2020-02-07 17:59:56 +0100

    ajout des classes player, Game, Chest et RandomArm

---
+++ Antic Fight-Last Version/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  Antic Fight-Last Version
+//
+//  Created by macmini-Armelle on 07/02/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
---
+++ Antic Fight-Last Version/RandomArm.swift
@@ -0,0 +1,15 @@
+//
+//  RandomArm.swift
+//  Antic Fight-Last Version
+//
+//  Created by macmini-Armelle on 07/02/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class RandomArm: Weapon {
+    init() {
+        super.init (damage : 10)
+        
+    }
+}
